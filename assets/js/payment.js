0fe3f669d23314221f7a26b3186a5e30faea752b
ameektm <ameektm@gmail.com>
2022-02-17 10:40:58 +0530

    Add all my files

---
+++ assets/js/payment.js
@@ -0,0 +1,25 @@
+function credit_form(){
+    document.getElementById('credit-form').style.display = "block";
+    document.getElementById('esewa_Id').style.display = "none";
+    document.getElementById('Ime_Id').style.display = "none";
+    document.getElementById('Cash_ON').style.display = "none";
+   
+ }
+function Esewa_form() {
+    document.getElementById('credit-form').style.display = "none";
+    document.getElementById('esewa_Id').style.display = "block";
+    document.getElementById('Ime_Id').style.display = "none";
+    document.getElementById('Cash_ON').style.display = "none";
+ }
+function Ime_form() {
+    document.getElementById('credit-form').style.display = "none";
+    document.getElementById('esewa_Id').style.display = "none";
+    document.getElementById('Ime_Id').style.display = "block";
+    document.getElementById('Cash_ON').style.display = "none";
+ }
+function COD_form() {
+    document.getElementById('credit-form').style.display = "none";
+    document.getElementById('esewa_Id').style.display = "none";
+    document.getElementById('Ime_Id').style.display = "none";
+    document.getElementById('Cash_ON').style.display = "block";
+ }
